f33f63ea8839ac5b4cb886f963b00c4bcd821550
AakashShindeHelsinki <shinde.aakashravindra@helsinki.fi>
2025-03-21 11:53:29 +0200

    Trying to create pip install failed

---
+++ 4X4Dataset/__main__.py
@@ -0,0 +1,15 @@
+import dataval_constructor
+
+
+def main():
+    #Default Values
+    number_of_value = 2000
+    pixel_diff = 0.5 # Greater than 0, Less than 1
+    greator_val = None # Linear / Diagonal
+    data_percent_split=0.5 # Between 0 and 1 needs greator val argument else useless
+    shuffle = False # True / False
+    X,y = dataval_constructor.dataval_constructor(number_of_value, pixel_diff, greator_val, data_percent_split, shuffle)
+    return X,y
+
+if __name__ == "__main__":
+    main()
